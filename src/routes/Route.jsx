178fdcae5a886d663a4630549a1cb6f1e2da6c8d
iamtonmoy0 <tonmoy2code@gmail.com>
2023-06-09 02:47:52 -0400

    feat: sign up ,sign in components added and firebase auth created .

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import * as RoutePath from './RoutePath'
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
 {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
 		path:RoutePath.HOME,
 		element:<Home/>
 
+		},
+		{
+			path:RoutePath.SIGNIN,
+			element:<Login/>
+		},
+		{
+			path:RoutePath.SIGNUP,
+			element:<SignUp/>
 		}
 	]
 }
--- src/routes/RoutePath.js
+++ src/routes/RoutePath.js
@@ -1,1 +1,3 @@
 export const HOME = '/';
+export const SIGNIN = '/login';
+export const SIGNUP = '/register';
